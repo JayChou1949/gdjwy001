59055e89cf20922f22f8f9e03fa5bcbba1e6c6bc
xuqiuping <xuqp1615@163.com>
2020-06-29 17:15:24 +0800

    pojo类修改

--- src/main/java/com/upd/hwcloud/bean/entity/application/manage/ApplicationManage.java
+++ src/main/java/com/upd/hwcloud/bean/entity/application/manage/ApplicationManage.java
@@ -1,21 +1,20 @@
 package com.upd.hwcloud.bean.entity.application.manage;
 
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
 import com.baomidou.mybatisplus.extension.activerecord.Model;
-import com.baomidou.mybatisplus.annotation.TableId;
-import com.baomidou.mybatisplus.annotation.TableField;
-import lombok.Data;
 
+import java.io.Serializable;
 
 /**
  * <p>
  * 应用运营管理
  * </p>
  *
- * @author lqm
+ * @author xqp
  * @since 2020-06-29
  */
-@Data
 @TableName("TB_APPLICATION_MANAGE")
 public class ApplicationManage extends Model<ApplicationManage> {
 
@@ -25,16 +24,10 @@ public class ApplicationManage extends Model<ApplicationManage> {
     private String id;
 
         /**
-     * 地市
+     * 地市/警种
      */
-         @TableField("AREA")
-    private String area;
-
-        /**
-     * 警种
-     */
-         @TableField("POLICE")
-    private String police;
+         @TableField("AREA_OR_POLICE")
+    private String areaOrPolice;
 
         /**
      * 用户配额数
@@ -79,4 +72,104 @@ public class ApplicationManage extends Model<ApplicationManage> {
     private Long seriousViolationNum;
 
 
+    public String getId() {
+        return id;
+    }
+
+    public ApplicationManage setId(String id) {
+        this.id = id;
+        return this;
+    }
+
+    public String getAreaOrPolice() {
+        return areaOrPolice;
+    }
+
+    public ApplicationManage setAreaOrPolice(String areaOrPolice) {
+        this.areaOrPolice = areaOrPolice;
+        return this;
+    }
+
+    public Long getUserQuotaNum() {
+        return userQuotaNum;
+    }
+
+    public ApplicationManage setUserQuotaNum(Long userQuotaNum) {
+        this.userQuotaNum = userQuotaNum;
+        return this;
+    }
+
+    public Long getUserTotal() {
+        return userTotal;
+    }
+
+    public ApplicationManage setUserTotal(Long userTotal) {
+        this.userTotal = userTotal;
+        return this;
+    }
+
+    public Long getPoliceUserNum() {
+        return policeUserNum;
+    }
+
+    public ApplicationManage setPoliceUserNum(Long policeUserNum) {
+        this.policeUserNum = policeUserNum;
+        return this;
+    }
+
+    public Long getNoPoliceUserNum() {
+        return noPoliceUserNum;
+    }
+
+    public ApplicationManage setNoPoliceUserNum(Long noPoliceUserNum) {
+        this.noPoliceUserNum = noPoliceUserNum;
+        return this;
+    }
+
+    public Long getAvailableQuotaNum() {
+        return availableQuotaNum;
+    }
+
+    public ApplicationManage setAvailableQuotaNum(Long availableQuotaNum) {
+        this.availableQuotaNum = availableQuotaNum;
+        return this;
+    }
+
+    public Long getViolationNum() {
+        return violationNum;
+    }
+
+    public ApplicationManage setViolationNum(Long violationNum) {
+        this.violationNum = violationNum;
+        return this;
+    }
+
+    public Long getSeriousViolationNum() {
+        return seriousViolationNum;
+    }
+
+    public ApplicationManage setSeriousViolationNum(Long seriousViolationNum) {
+        this.seriousViolationNum = seriousViolationNum;
+        return this;
+    }
+
+    @Override
+    protected Serializable pkVal() {
+        return this.id;
+    }
+
+    @Override
+    public String toString() {
+        return "ApplicationManage{" +
+        "id=" + id +
+        ", areaOrPolice=" + areaOrPolice +
+        ", userQuotaNum=" + userQuotaNum +
+        ", userTotal=" + userTotal +
+        ", policeUserNum=" + policeUserNum +
+        ", noPoliceUserNum=" + noPoliceUserNum +
+        ", availableQuotaNum=" + availableQuotaNum +
+        ", violationNum=" + violationNum +
+        ", seriousViolationNum=" + seriousViolationNum +
+        "}";
+    }
 }
